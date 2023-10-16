b59590941235823543a2a4b430005e0fc756872b
KarinaGSerrano <karina.g.serrano@gmail.com>
2023-10-16 19:00:07 -0300

    function render people

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 "use strict";
 const containerCards = document.querySelector(".container-cards");
+const urlFirst = "https://swapi.dev/api/people/?page=1";
 
 // --------------------------------------------------------------------------------------
 
-async function swAPI(numPage) {
-  return fetch(`https://swapi.dev/api/people/?page=${numPage}`).then(
-    (response) => response.json()
-  );
+async function swApiPeople(url) {
+  return fetch(url).then((response) => response.json());
+}
+
+// --------------------------------------------------------------------------------------
+
+async function renderPeopleCards(cantidad) {
+  let arrPeople = [];
+  let url = urlFirst;
+
+  do {
+    const people = await swApiPeople(url);
+    people.results.forEach((person) => {
+      arrPeople.push(person);
+    });
+    url = people.next;
+  } while (arrPeople.length <= cantidad);
+
+  arrPeople.forEach((person, i) => {
+    if (i < cantidad) {
+      renderCard(
+        "https://img.freepik.com/foto-gratis/vista-nave-espacial-aspecto-futurista_23-2150675497.jpg?w=1380&t=st=1697487761~exp=1697488361~hmac=c6de712bc1b4145a068d01e2ad9a3dd5f3a24405b34a44e8cc48fb14da24c062",
+        person.name,
+        person.eye_color
+      );
+    }
+  });
 }
 
 // --------------------------------------------------------------------------------------
@@ -34,3 +58,8 @@ function renderCard(img, title, description) {
                           </div>`;
   containerCards.insertAdjacentHTML("beforeend", templateCards);
 }
+
+// --------------------------------------------------------------------------------------
+
+// comportamiento default APP:
+// renderPeopleCards(25);
