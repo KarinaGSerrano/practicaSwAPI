917ef802d61b8e85a458627b3de4a00eb48d7d93
KarinaGSerrano <karina.g.serrano@gmail.com>
2023-10-16 15:47:08 -0300

    Render card

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const containerCards = document.querySelector(".container-cards");
+
+function renderCard(img, title, description) {
+  const templateCards = `  <div class="pt-4 col-lg-4 col-md-6 col-sm-12">
+                            <div class="card text-center">
+                              <img
+                                src="${img}"
+                                class="card-img-top"
+                                alt="..."
+                              />
+                              <div class="card-body">
+                                <h2 class="card-title">${title}</h2>
+                                <p class="card-description">${description}</p>
+                                <button
+                                  type="button"
+                                  class="btn btn-style"
+                                  data-bs-toggle="modal"
+                                  data-bs-target="#staticBackdrop"
+                                >
+                                  Ver más
+                                </button>
+                              </div>
+                            </div>
+                          </div>`;
+  containerCards.insertAdjacentHTML("beforeend", templateCards);
+}
